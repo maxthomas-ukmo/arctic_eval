321cb601210e4ae723269eb69820684563c5afd8
Max Thomas <max.thomas@metoffice.gov.uk>
2025-07-28 16:33:55 +0100

    Updated run scripts to have new env name and no hardcoded paths.

--- code/run_arctic_ocean.sh
+++ code/run_arctic_ocean.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -l
+cd "$(dirname "$0")"
 #SBATCH --mem=20G
 #SBATCH --ntasks=8
 #SBATCH --output=./MWOA-test.log
 #SBATCH --time=60
 #SBATCH --export=NONE
-cd /home/users/max.thomas/projects/esmvaltool/development
-module load scitools/community/esmvaltool/2.10.0
-esmvaltool run recipe_arctic_ocean.yml --config_file ./config-user.yml
- 
+conda activate arctic_eval
+esmvaltool run recipe_arctic_ocean.yml --config_file ../config/config-user.yml
--- code/run_arctic_seaice.sh
+++ code/run_arctic_seaice.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -l
-#SBATCH --mem=20G
+cd "$(dirname "$0")"
+#SBATCH --mem=100G
 #SBATCH --ntasks=8
-#SBATCH --output=./new_com_test.log
+#SBATCH --output=./seaice_eval.log
 #SBATCH --time=60
 #SBATCH --export=NONE
-#cd ~mathomas/Projects/esmvaltool/arctic_ocean_recipe/cmip6_eval
-cd /home/users/max.thomas/projects/esmvaltool/development
-#module load scitools/community/esmvaltool/2.10.0
-conda activate esmvaltool-2.11.0-strait_flux
-esmvaltool run recipe_arctic_seaice.yml --config_file ./config-user.yml
- 
+conda activate arctic_eval
+esmvaltool run recipe_arctic_seaice.yml --config_file ../config/config-user.yml
